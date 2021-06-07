6b060bcf98498f74979613b57030a35d76697dfa
2019211001000807-yanjunlong <616065464@qq.com>
2021-06-07 11:43:33 +0800

    HK-Week 14 Commit

--- src/main/java/com/yanjunlong/controller/UpdateUserServlet.java
+++ src/main/java/com/yanjunlong/controller/UpdateUserServlet.java
@@ -3,17 +3,21 @@ package com.yanjunlong.controller;
 import com.yanjunlong.dao.Userdao;
 import com.yanjunlong.model.User;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.zip.Inflater;
 
-@WebServlet(name = "UpdateUserServlet", value = "/UpdateUser")
+@WebServlet(name = "UpdateUserServlet",value="/updateUser")
 public class UpdateUserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("in");
@@ -34,15 +38,20 @@ public class UpdateUserServlet extends HttpServlet {
         User u=new User(Integer.valueOf(id),username,password,mail,sex,birth);
         Userdao userDao=new Userdao();
         try {
-            userDao.updateUser((Connection) getServletContext().getAttribute("con"),u);
+            userDao.updateUser((Connection)getServletContext().getAttribute("con"),u);
+            HttpSession session=request.getSession();
+            session.setMaxInactiveInterval(60*60);
+            session.setAttribute("user",u);
+
         } catch (SQLException e) {
             System.out.println(e);
         }
         System.out.println(birth);
-        request.getRequestDispatcher("WEB-INF/views/userInfo.jsp").forward(request,response);
+        request.getRequestDispatcher("accountDetails").forward(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("herhe");
         request.getRequestDispatcher("WEB-INF/views/updateUser.jsp").forward(request,response);
     }
 }
